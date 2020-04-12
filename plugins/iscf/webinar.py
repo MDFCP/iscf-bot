7ac5223addee74f68ca96c221452dc71a1d9caf6
Abhishek Upperwal <a.upperwal@gmail.com>
2020-04-12 16:56:51 +0530

    added timezone

--- plugins/iscf/webinar.py
+++ plugins/iscf/webinar.py
@@ -15,7 +15,7 @@ def submit(db, args):
         db[WB_INDEX] = []
     
     try:
-        parsed_date = datetime.strptime(args[1], '%d/%m/%Y %I:%M %p')
+        parsed_date = datetime.strptime(args[1] + ' +0530', '%d/%m/%Y %I:%M %p %z')
     except:
         return 'Date should be formatted as <DD/MM/YYYY hh:mm AM/PM>. eg: "12/04/2020 3:05 PM"'
 
