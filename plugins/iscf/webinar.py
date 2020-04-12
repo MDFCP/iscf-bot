b7444122830270e1a24f94e4f9c51770559520ca
Abhishek Upperwal <a.upperwal@gmail.com>
2020-04-12 17:55:02 +0530

    added timezone info

--- plugins/iscf/webinar.py
+++ plugins/iscf/webinar.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import html
 import heapq
+from pytz import timezone
 from datetime import datetime, timedelta
 
 WB_INDEX = 'webinars'
@@ -15,11 +16,13 @@ def submit(db, args):
         db[WB_INDEX] = []
     
     try:
-        parsed_date = datetime.strptime(args[1], '%d/%m/%Y %I:%M %p')
+        parsed_date = datetime.strptime(args[1] + ' +0530', '%d/%m/%Y %I:%M %p %z')
     except:
         return 'Date should be formatted as <DD/MM/YYYY hh:mm AM/PM>. eg: "12/04/2020 3:05 PM"'
 
-    if parsed_date < datetime.now():
+    now = datetime.now(timezone('Asia/Kolkata'))
+
+    if parsed_date < now:
         return 'Is this webinar a ghost? Buddy, check the date. You are living in the past.'
 
     temp_heap = db[WB_INDEX]
@@ -31,7 +34,7 @@ def submit(db, args):
 
     db[WB_INDEX] = temp_heap
     
-    return html.escape('Curent Date: ' + datetime.strftime(datetime.now(), '%d %b, %Y %I:%M %p') + '\nThanks!! You submitted **' + args[0] + '** which is scheduled at **' + datetime.strftime(parsed_date, '%d %b, %Y %I:%M %p') + '**.')
+    return html.escape('Curent Date: ' + datetime.strftime(now, '%d %b, %Y %I:%M %p') + '\nThanks!! You submitted **' + args[0] + '** which is scheduled at **' + datetime.strftime(parsed_date, '%d %b, %Y %I:%M %p') + '**.')
 
 # args: !wb get 3
 def get(db, args):
@@ -55,7 +58,7 @@ def get(db, args):
         if cnt >= num or len(heap) == 0 or cnt >= len(heap):
             break
         
-        if heap[0][0] < datetime.now():
+        if heap[0][0] < datetime.now(timezone('Asia/Kolkata')):
             heapq.heappop(heap)
         else:
             res = display_format(res, heap[cnt][1]['link'], heap[cnt][1]['name'], heap[cnt][0])
@@ -78,7 +81,7 @@ def notify(self):
             break
         
         cnt = cnt + 1
-        if heap[0][0] < (datetime.now() + timedelta(minutes = 10)):
+        if heap[0][0] < (datetime.now(timezone('Asia/Kolkata')) + timedelta(minutes = 10)):
             self.send(self.build_identifier("#bot-test"), html.escape(display_format('', heap[0][1]['link'], heap[0][1]['name'], heap[0][0])))
             heapq.heappop(heap)
     
